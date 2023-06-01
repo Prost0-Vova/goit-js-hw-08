43b574216fa5972e91e4002210dbcf3c69e444d8
Prost0-Vova <122817734+Prost0-Vova@users.noreply.github.com>
2023-06-01 19:50:15 +0200

    а

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const createGalleryImages = ({
+  preview,
+  original,
+  description,
+}) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+
+const galleryImagesList = galleryItems.map(el => createGalleryImages(el));
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryImagesList.join(''));
+
+const captions = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
+
+
