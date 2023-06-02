0252ba114e4abfbbc945c2dd96a11895f3af1896
Prost0-Vova <122817734+Prost0-Vova@users.noreply.github.com>
2023-06-02 15:44:01 +0200

    d

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const vimeoPlayer = new Player('iframe', {
+  title: true,
+  autoplay: true,
+  loop: false,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+const timeKey = 'videoplayer-current-time';
+
+const getCurrentTime = function (currentTime) {
+    
+    const seconds = currentTime.seconds;
+    localStorage
+}
+
+
+vimeoPlayer.on('timeupdate', throttle(getCurrentTime, 1000));
